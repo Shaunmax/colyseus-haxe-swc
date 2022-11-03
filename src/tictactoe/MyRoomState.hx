ec12a20998b5cfff9c946209b8aa37c8be3e40f4
Playkia <playkia@gmail.com>
2022-11-03 22:46:16 +0530

    Added example for Adobe AIR

---
+++ src/tictactoe/MyRoomState.hx
@@ -0,0 +1,31 @@
+// 
+// THIS FILE HAS BEEN GENERATED AUTOMATICALLY
+// DO NOT CHANGE IT MANUALLY UNLESS YOU KNOW WHAT YOU'RE DOING
+// 
+// GENERATED USING @colyseus/schema 1.0.41
+// 
+
+package tictactoe;
+import io.colyseus.serializer.schema.Schema;
+import io.colyseus.serializer.schema.types.*;
+@:keep
+class MyRoomState extends Schema {
+	@:type("string")
+	public var currentTurn: String = "";
+
+	@:type("map", "boolean")
+	public var players: MapSchema<Bool> = new MapSchema<Bool>();
+
+	@:type("array", "number")
+	public var board: ArraySchema<Dynamic> = new ArraySchema<Dynamic>();
+
+	@:type("string")
+	public var winner: String = "";
+
+	@:type("string")
+	public var winline: String = "";
+
+	@:type("boolean")
+	public var draw: Bool = false;
+
+}
